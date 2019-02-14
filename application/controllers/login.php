e63288fcf64af46d5210a14d5d5038eb25ee96b4
matoom123 <45991126+matoom123@users.noreply.github.com>
2019-02-14 23:13:37 +0700

    lo

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,15 +1,48 @@
-<?php
+<?php 
+session_start();
+        if(isset($_POST['Username'])){
+				//connection
+                  include("connection.php");
+				//รับค่า user & password
+                  $Username = $_POST['Username'];
+                  $Password = md5($_POST['Password']);
+				//query 
+                  $sql="SELECT * FROM user Where Username='".$username."' and Password='".$password."' ";
 
-class login extends CI_Controller{
+                  $result = mysqli_query($con,$sql);
+				
+                  if(mysqli_num_rows($result)==1){
 
-    function __construct(){
-        parent::__construct();
-    }
+                      $row = mysqli_fetch_array($result);
 
-    function login1(){
-        $this->load->view('login');
-        
-    }
-}
+                      $_SESSION["UserID"] = $row["ID"];
+                      $_SESSION["User"] = $row["Firstname"]." ".$row["Lastname"];
+                      $_SESSION["create_meeting"] = $row["create_meeting"];
+
+                      if($_SESSION["create_meeting"]=="Y"){ //ถ้าเป็น admin ให้กระโดดไปหน้า admin_page.php
+
+                        Header("Location: calendar.php");
+
+                      }
+
+                      if ($_SESSION["create_meeting"]=="N"){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
+
+                        Header("Location: calendar.php");
+
+                      }
+
+                  }else{
+                    echo "<script>";
+                        echo "alert(\" user หรือ  password ไม่ถูกต้อง\");"; 
+                        echo "window.history.back()";
+                    echo "</script>";
+
+                  }
+
+        }else{
 
 
+             Header("Location: form.php"); //user & password incorrect back to login again
+
+        }
+?>
