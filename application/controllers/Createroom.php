837662293b14dc6e0af6ceb55d03bdfb5361a64d
best1302 <33709504+best1302@users.noreply.github.com>
2019-02-14 14:57:41 +0700

    asdas

---
+++ application/controllers/Createroom.php
@@ -0,0 +1,39 @@
+<?php
+
+class Createroom extends CI_Controller{
+public function __construct(){
+        parent::__construct();
+        
+    }
+    public  function addroom(){
+
+        $this->load->view('header');
+        $this->load->view('convene');
+
+        $uid=$this->input->post('uid');
+        $username=$this->input->post('username');
+        $password=$this->input->post('password');
+        $firstname=$this->input->post('firstname');
+        $lastname=$this->input->post('lastname');
+        $picture=$this->input->post('picture');
+        $email=$this->input->post('email');
+        $phone=$this->input->post('mobile');
+        $prefic_name=$this->input->post('preficname');
+  
+        $data = array(     
+          'uid' => $uid,
+          'username' => $username,
+          'password' => $password,
+          'firstname' => $firstname,
+          'lastname' => $lastname,
+          //'picture' => '',
+          'email' => $email,
+          'phone' => $phone,
+          'prefic_name' => $prefic_name     
+  );
+  $this->db->insert('user', $data);  //เลือกinsert ลงในตาราง User 
+     
+
+     }
+}
+
