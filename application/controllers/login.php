c2f59fd1b211d7a61168748605b4b303ed1b02ea
matoom123 <45991126+matoom123@users.noreply.github.com>
2019-02-16 21:56:54 +0700

    log

---
+++ application/controllers/login.php
@@ -0,0 +1,15 @@
+<?php
+
+class login extends CI_Controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function login1(){
+        $this->load->view('login');
+        
+    }
+}
+
+
