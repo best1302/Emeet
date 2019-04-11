152fbaab3e79b2b3fc10e5eead0811e9c8bbe520
matoom123 <45991126+matoom123@users.noreply.github.com>
2019-04-12 01:20:24 +0700

    55

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,12 +5,32 @@ class Main extends CI_Controller{
     function __construct(){
         parent::__construct();
     }
-
     function login(){
+        if($post = $this->input->post()){
+            extract($post);
+            $sql = "select * from user where username = '$username' and password = '$password'";
+            $ret = $this->db->query($sql);
+            if($ret->num_rows()){
+                $data_ret = $ret->result();
+                $this->session->set_userdata('uid',$data_ret[0]->uid);
+                $this->session->set_userdata('firstname',$data_ret[0]->firstname);
+                $this->session->set_userdata('lastname',$data_ret[0]->lastname);
+            }else{
+                $data['message'] = "mother fucker";
+            }
+        }
+        $this->load->view('header');
         $this->load->view('login');
+
         
     }
+    public function logout(){
+        $this->session->session_destroy();
+        redirect(base_url."index.php/main/login");
+    }
 
+
+    
     public function register(){
         
         if (isset($_POST['register'])) {
@@ -25,9 +45,7 @@ class Main extends CI_Controller{
        
               if ($this->form_validation->run() == TRUE) {
                 echo 'form validated';
-
-                $data = array(     
-                    
+                $data = array(               
                     'username' => $_POST['username'],
                     'password' => $_POST['password'],
                     'firstname' => $_POST['firstname'],
@@ -36,15 +54,12 @@ class Main extends CI_Controller{
                     'email' => $_POST['email'],
                     'phone' => $_POST['phone'],
                     'prefic_name' => $_POST['preficname']  );
-                    
                     $this->db->insert('user', $data);
-
               
               redirect("main/register","refresh");
             }
         }
         //เรียกหน้า
-        
         $this->load->view('header');  
         $this->load->view('adduser');
     }
